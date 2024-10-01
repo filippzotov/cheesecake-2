11af0d552aa66df525e7443c0c4fef7af6c3b2b8
Filipp Zotov <filippzotov1998@gmail.com>
2024-10-01 11:04:57 +0200

    Refactor import statements in bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
+from interactions import SlashCommand, SlashContext
 from play_audio import play_youtube_audio
 import asyncio
 
